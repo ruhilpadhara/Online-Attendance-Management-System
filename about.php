bbbb638ecfa118c8a02e818149aca02ad50fd84b
ruhilpadhara <9999padhara@gmail.com>
2023-03-11 10:24:21 +0530

    Initial commit

---
+++ about.php
@@ -0,0 +1,70 @@
+<?php
+  session_start();
+  $isIndex = 0;
+  if(!(array_key_exists('teacher_id',$_SESSION) && isset($_SESSION['teacher_id']))) {
+    session_destroy();
+    if(!$isIndex) header('Location: index.php');
+  }
+?>
+<?php include 'php/node_class.php'; ?>
+<html>
+ <head>
+  <link rel="stylesheet" href="css/style.css"/>
+  <title>About</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/highcharts.js"></script>
+  <script src="js/highcharts-exporting.js"></script>
+  <!-- Custom styles for this template -->
+    <link href="navbar-fixed-top.css" rel="stylesheet">
+ </head>
+ <body>
+   <!-- Fixed navbar -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <br>
+          <a class="navbar-brand" href="index.php" style="font-size:28px;">CHARUSAT</a><br />
+          <a class="navbar-brand" href="index.php">Online Attendance</a>
+        </div>
+
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+			<a href="teacher.php">Dashboard</a></li>
+			<li><a href="profile.php">Profile</a></li>
+			<li><a href="student.php">Students</a></li>
+           	<li><a href="statistics.php">Statistics</a></li>
+			<li class="active"><a href="about.php">About</a></li>
+            	<li><a href="contact.php">Contact</a></li>
+			<li><a href="description.php">Description</a></li>
+			<li><a href="logout.php">Logout</a>
+		</li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav></br></br></br></br>
+  </div>
+<br>
+
+<h3><div>
+      <dl class="dl-horizontal">
+        <dd><h1 style="font-size: 50px;"><strong><b>Charotar University of Science and Technology</b></strong></h1></dd>
+        <dd><h3>139,<br>CHARUSAT Campus, Highway, Off,<br>Nadiad - Petlad Rd,<br>Changa, Gujarat 388421</h3></dd>
+<br>
+
+<dd><h2><strong>Our Location:</strong></h2></dd>
+<dd>
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14733.808145082363!2d72.8205!3d22.599589!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e50c43cdea6c7%3A0x5074fe9e0c1c8bd!2sCharotar%20University%20of%20Science%20and%20Technology%20(CHARUSAT)!5e0!3m2!1sen!2sin!4v1618681664360!5m2!1sen!2sin" width="650" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe></dd><br />
+</div>
+</h3>
+</body>
+</html>
